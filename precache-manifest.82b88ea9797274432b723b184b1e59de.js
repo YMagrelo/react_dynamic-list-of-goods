1dd5e216c38bbdde6498951c153aa2c2d950198a
Yaroslav Magrelo <slava.magrelo@gmail.com>
2020-03-02 23:40:10 +0200

    Updates

---
+++ precache-manifest.82b88ea9797274432b723b184b1e59de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd6d7f78c46ddd0a176750a46cee26f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6ffddb1dda158a3bf4b4",
+    "url": "./static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "b9ab41a1cd313e403f95",
+    "url": "./static/js/2.a0138fc0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.a0138fc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ffddb1dda158a3bf4b4",
+    "url": "./static/js/main.5adff89c.chunk.js"
+  },
+  {
+    "revision": "2e3a6f278567b71a3edc",
+    "url": "./static/js/runtime-main.5e540f5e.js"
+  }
+]);
